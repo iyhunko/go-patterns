6237eb842929bdcbdf63d694dab4b19adc06efec
Ivan Hunko <i.hunko@sap.com>
2025-07-10 11:55:23 +0300

    channels: pipeline pattern

---
+++ channels-pipeline-pattern/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "time"
+import "fmt"
+
+func main() {
+	reader(double(writer()))
+}
+
+func writer() <-chan int {
+	ch := make(chan int)
+	go func() {
+		for i := range 10 {
+			ch <- i + 1
+		}
+		close(ch)
+	}()
+
+	return ch
+}
+
+func double(ch <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for v := range ch {
+			time.Sleep(500 * time.Millisecond) // Simulate some processing delay
+			out <- v * 2
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+func reader(ch <-chan int) {
+	for v := range ch {
+		fmt.Println(v)
+	}
+}
